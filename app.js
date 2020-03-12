7cd675b17c975dd0107574c66ce6841116f330b2
Meryem Kömür <meryemkomur@Meryems-MacBook-Pro.local>
2020-03-11 19:07:29 -0500

    final

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+// public folder
+app.use(express.static('public'));
+
+//routes
+
+app.use(require('./routes/blogs'))
+app.use(require('./routes/editblogs'))
+
+
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+});
